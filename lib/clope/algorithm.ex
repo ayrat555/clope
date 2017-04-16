1020d0fddff5683055eb8477be9fdee68cf3ff7b
Ayrat Badykov <ayratin555@gmail.com>
2017-04-16 18:16:12 +0300

    change reduce to recursion

--- lib/clope/algorithm.ex
+++ lib/clope/algorithm.ex
@@ -3,6 +3,12 @@ defmodule Clope.Algorithm do
   alias Clope.Cluster
   alias Clope.Partition
 
+  def clusters(transactions, repulsion) do
+    initial_clusters = transactions |> initialize_clusters(repulsion)
+
+
+  end
+
   def initialize_clusters(transactions, repulsion) do
     partition = %Partition{}
 
@@ -12,30 +18,54 @@ defmodule Clope.Algorithm do
     end)
   end
 
-  def add_to_partition(
+  defp add_to_partition(
       transaction,
       %Partition{clusters: clusters} = partition,
       repulsion) do
-    new_cluster_delta = Profit.delta(%Cluster{}, transaction, repulsion)
+
+    clusters
+    |> max_profit_cluster(transaction, repulsion)
+    |> add_to_cluster(partition, transaction)
+  end
+
+  defp add_to_cluster({_delta, nil}, partition, transaction) do
     new_cluster = Cluster.add_transaction(%Cluster{}, transaction)
 
-    {best_delta, best_cluster} =
-      clusters
-      |> Enum.reduce({new_cluster_delta, nil}, fn(cluster, {best_delta, best_cluster}) ->
-        delta = Profit.delta(cluster, transaction, repulsion)
-        if best_delta < delta do
-          {delta, cluster}
-        else
-          {best_delta, best_cluster}
-        end
-      end)
+    Partition.add_cluster(partition, new_cluster)
+  end
 
-    case {best_delta, best_cluster} do
-      {^new_cluster_delta, nil} ->
-        Partition.add_cluster(partition, new_cluster)
-      {delta, cluster} ->
-        new_cluster = Cluster.add_transaction(cluster, transaction)
-        Partition.replace_cluster(partition, cluster, new_cluster)
-    end
+  defp add_to_cluster({_delta, cluster}, partition, transaction) do
+    new_cluster = Cluster.add_transaction(cluster, transaction)
+
+    Partition.replace_cluster(partition, cluster, new_cluster)
+  end
+
+  defp max_profit_cluster(clusters, transaction, repulsion) do
+    new_cluster_delta = Profit.delta(%Cluster{}, transaction, repulsion)
+
+    max_profit_cluster(clusters, {new_cluster_delta, nil}, transaction, repulsion)
+  end
+
+  defp max_profit_cluster([], {best_delta, best_cluster}, _transaction, _repulsion) do
+    {best_delta, best_cluster}
+  end
+
+  defp max_profit_cluster([cluster | []], {best_delta, best_cluster}, transaction, repulsion) do
+    delta = cluster |> Profit.delta(transaction, repulsion)
+
+    if best_delta < delta,
+      do: {delta, cluster},
+      else: {best_delta, best_cluster}
+  end
+
+  defp max_profit_cluster([cluster | tail], {best_delta, best_cluster}, transaction, repulsion) do
+    delta = cluster |> Profit.delta(transaction, repulsion)
+
+    {best_delta, best_cluster} =
+      if best_delta < delta,
+        do: {delta, cluster},
+        else: {best_delta, best_cluster}
+
+    max_profit_cluster(tail, {best_delta, best_cluster}, transaction, repulsion)
   end
 end
